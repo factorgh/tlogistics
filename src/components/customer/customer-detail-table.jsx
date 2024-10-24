40ad957d1d5402947e7c97c4a061ee29da9769ee
factorgh <burchellsbale@gmail.com>
2024-10-24 17:44:41 +0000

    push dated

--- src/components/customer/customer-detail-table.jsx
+++ src/components/customer/customer-detail-table.jsx
@@ -1,19 +1,6 @@
 import { Table } from "antd";
 const CustomerDetailTable = () => {
-  const dataSource = [
-    {
-      key: "1",
-      name: "Mike",
-      age: 32,
-      address: "10 Downing Street",
-    },
-    {
-      key: "2",
-      name: "John",
-      age: 42,
-      address: "10 Downing Street",
-    },
-  ];
+  const dataSource = [];
 
   const columns = [
     {
